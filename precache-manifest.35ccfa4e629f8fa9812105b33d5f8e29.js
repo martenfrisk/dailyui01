e742419f0c7ec853d003782e6565f1ef1771ac71
Mårten Frisk <marten.frisk@chasacademy.se>
2020-04-27 23:31:33 +0200

    Updates

---
+++ precache-manifest.35ccfa4e629f8fa9812105b33d5f8e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cfb7542854652f60ae2aecf378481c",
+    "url": "/dailyui01/index.html"
+  },
+  {
+    "revision": "9704f0f1fd479cbcb60a",
+    "url": "/dailyui01/static/css/main.b2445341.chunk.css"
+  },
+  {
+    "revision": "71f7886b90a510f7d70b",
+    "url": "/dailyui01/static/js/2.12346b60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dailyui01/static/js/2.12346b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9704f0f1fd479cbcb60a",
+    "url": "/dailyui01/static/js/main.c0b3c16d.chunk.js"
+  },
+  {
+    "revision": "baf13465be8627182dc4",
+    "url": "/dailyui01/static/js/runtime-main.e2282f42.js"
+  },
+  {
+    "revision": "6ab1510051e72d0b3ee0f5a028b88a98",
+    "url": "/dailyui01/static/media/graphic-1.6ab15100.svg"
+  }
+]);
